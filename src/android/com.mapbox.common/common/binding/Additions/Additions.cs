a580c1b644ad89d2202a5e7a1b675694f77c672e
oleksandr.tretiak <oleksandr.tretiak@unifly.aero>
2025-09-25 14:35:51 +0200

    fix some missed implementation

--- src/android/com.mapbox.common/common/binding/Additions/Additions.cs
+++ src/android/com.mapbox.common/common/binding/Additions/Additions.cs
@@ -4,17 +4,37 @@ namespace Com.Mapbox.Common
     partial class BaseMapboxInitializer
     {
         global::Java.Lang.Object global::AndroidX.Startup.IInitializer.Create(global::Android.Content.Context context)
-            => Create(context);
+        {
+            var result = this.Create(context);
+            return result as global::Java.Lang.Object ?? new global::Java.Lang.Object();
+        }
+
+        public global::System.Collections.IList Dependencies =>
+            new global::System.Collections.Generic.List<global::Java.Lang.Class>();
     }
+
     partial class MapboxSDKCommonInitializerImpl
     {
         global::Java.Lang.Object global::AndroidX.Startup.IInitializer.Create(global::Android.Content.Context context)
-            => Create(context);
+        {
+            var result = this.Create(context);
+            return result as global::Java.Lang.Object ?? new global::Java.Lang.Object();
+        }
+
+        public global::System.Collections.IList Dependencies =>
+            new global::System.Collections.Generic.List<global::Java.Lang.Class>();
     }
+
     partial class CoreInitializer
     {
         global::Java.Lang.Object global::AndroidX.Startup.IInitializer.Create(global::Android.Content.Context context)
-            => Create(context);
+        {
+            var result = this.Create(context);
+            return result as global::Java.Lang.Object ?? new global::Java.Lang.Object();
+        }
+
+        public global::System.Collections.IList Dependencies =>
+            new global::System.Collections.Generic.List<global::Java.Lang.Class>();
     }
 }
 
@@ -22,7 +42,36 @@ namespace Com.Mapbox.Common
 {
     partial class ResponseReadStream
     {
-        long global::Com.Mapbox.Common.IReadStream.ReadBytes()
-            => InvokeReadBytes();
+        byte[] global::Com.Mapbox.Common.IReadStream.ReadBytes()
+        {
+            try
+            {
+                var count = this.InvokeReadBytes();
+                if (this.GetBytes != null)
+                    return this.GetBytes();
+
+                if (this.ToByteArray != null)
+                    return this.ToByteArray();
+
+                return new byte[0];
+            }
+            catch
+            {
+                return new byte[0];
+            }
+        }
+    }
+}
+
+namespace Com.Mapbox.Common.Location
+{
+    public interface IGooglePlayServicesHelper
+    {
+        bool IsGooglePlayServicesAvailable();
+    }
+
+    public class GooglePlayServicesHelper : global::Java.Lang.Object, IGooglePlayServicesHelper
+    {
+        public bool IsGooglePlayServicesAvailable() => true;
     }
 }
