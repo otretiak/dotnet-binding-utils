c6092a91507e7868d71c8024dc9719a54d79e23c
oleksandr.tretiak <oleksandr.tretiak@unifly.aero>
2025-09-30 14:56:36 +0200

    try fix with Metadata

--- src/android/com.mapbox.common/common-ndk27/binding/Additions/Additions.cs
+++ src/android/com.mapbox.common/common-ndk27/binding/Additions/Additions.cs
@@ -1,14 +1,14 @@
-using System;
-using Android.Runtime;
+// using System;
+// using Android.Runtime;
 
-namespace Com.Mapbox.Common
-{
-    public partial class ResponseReadStream
-    {
-        // This bridges the gap between the generated InvokeReadBytes and the interface
-        byte[] Com.Mapbox.Common.IReadStream.ReadBytes()
-        {
-            return InvokeReadBytes();
-        }
-    }
-}
+// namespace Com.Mapbox.Common
+// {
+//     public partial class ResponseReadStream
+//     {
+//         // This bridges the gap between the generated InvokeReadBytes and the interface
+//         byte[] Com.Mapbox.Common.IReadStream.ReadBytes()
+//         {
+//             return InvokeReadBytes();
+//         }
+//     }
+// }
