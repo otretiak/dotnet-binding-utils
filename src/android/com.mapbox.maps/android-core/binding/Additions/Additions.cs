3b49d2489fc5b99ff9b3b469e3b2e75a0f3a945f
oleksandr.tretiak <oleksandr.tretiak@unifly.aero>
2025-10-01 16:39:39 +0200

    fix maps MapboxMapsInitializerImpl

--- src/android/com.mapbox.maps/android-core/binding/Additions/Additions.cs
+++ src/android/com.mapbox.maps/android-core/binding/Additions/Additions.cs
@@ -4,6 +4,12 @@ namespace Com.Mapbox.Maps.Loader
     partial class MapboxMapsInitializerImpl
     {
         global::Java.Lang.Object global::AndroidX.Startup.IInitializer.Create(global::Android.Content.Context context)
-            => Create(context);
+        {
+            var result = this.Create(context);
+            return result as global::Java.Lang.Object ?? new global::Java.Lang.Object();
+        }
+
+        public global::System.Collections.IList Dependencies =>
+            new global::System.Collections.Generic.List<global::Java.Lang.Class>();
     }
 }
