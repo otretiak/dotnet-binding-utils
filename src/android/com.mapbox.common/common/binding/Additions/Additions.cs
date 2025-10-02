5ed26024c3bf44ec94a1583e20a924fc420bd68b
oleksandr.tretiak <oleksandr.tretiak@unifly.aero>
2025-10-02 13:32:36 +0200

    revert additions changes

--- src/android/com.mapbox.common/common/binding/Additions/Additions.cs
+++ src/android/com.mapbox.common/common/binding/Additions/Additions.cs
@@ -4,37 +4,17 @@ namespace Com.Mapbox.Common
     partial class BaseMapboxInitializer
     {
         global::Java.Lang.Object global::AndroidX.Startup.IInitializer.Create(global::Android.Content.Context context)
-        {
-            var result = this.Create(context);
-            return result as global::Java.Lang.Object ?? new global::Java.Lang.Object();
-        }
-
-        public global::System.Collections.IList Dependencies =>
-            new global::System.Collections.Generic.List<global::Java.Lang.Class>();
+            => Create(context);
     }
-
     partial class MapboxSDKCommonInitializerImpl
     {
         global::Java.Lang.Object global::AndroidX.Startup.IInitializer.Create(global::Android.Content.Context context)
-        {
-            var result = this.Create(context);
-            return result as global::Java.Lang.Object ?? new global::Java.Lang.Object();
-        }
-
-        public global::System.Collections.IList Dependencies =>
-            new global::System.Collections.Generic.List<global::Java.Lang.Class>();
+            => Create(context);
     }
-
     partial class CoreInitializer
     {
         global::Java.Lang.Object global::AndroidX.Startup.IInitializer.Create(global::Android.Content.Context context)
-        {
-            var result = this.Create(context);
-            return result as global::Java.Lang.Object ?? new global::Java.Lang.Object();
-        }
-
-        public global::System.Collections.IList Dependencies =>
-            new global::System.Collections.Generic.List<global::Java.Lang.Class>();
+            => Create(context);
     }
 }
 
@@ -43,35 +23,6 @@ namespace Com.Mapbox.Common
     partial class ResponseReadStream
     {
         byte[] global::Com.Mapbox.Common.IReadStream.ReadBytes()
-        {
-            try
-            {
-                var count = this.InvokeReadBytes();
-                if (this.GetBytes != null)
-                    return this.GetBytes();
-
-                if (this.ToByteArray != null)
-                    return this.ToByteArray();
-
-                return new byte[0];
-            }
-            catch
-            {
-                return new byte[0];
-            }
-        }
-    }
-}
-
-namespace Com.Mapbox.Common.Location
-{
-    public interface IGooglePlayServicesHelper
-    {
-        bool IsGooglePlayServicesAvailable();
-    }
-
-    public class GooglePlayServicesHelper : global::Java.Lang.Object, IGooglePlayServicesHelper
-    {
-        public bool IsGooglePlayServicesAvailable() => true;
+            => Create(context);
     }
 }
--- src/android/com.mapbox.maps/android-core/binding/Additions/Additions.cs
+++ src/android/com.mapbox.maps/android-core/binding/Additions/Additions.cs
@@ -4,12 +4,6 @@ namespace Com.Mapbox.Maps.Loader
     partial class MapboxMapsInitializerImpl
     {
         global::Java.Lang.Object global::AndroidX.Startup.IInitializer.Create(global::Android.Content.Context context)
-        {
-            var result = this.Create(context);
-            return result as global::Java.Lang.Object ?? new global::Java.Lang.Object();
-        }
-
-        public global::System.Collections.IList Dependencies =>
-            new global::System.Collections.Generic.List<global::Java.Lang.Class>();
+            => Create(context);
     }
 }
