260fd992549bceeede84129089a3671fd118c868
oleksandr.tretiak <oleksandr.tretiak@unifly.aero>
2025-10-02 11:13:56 +0200

    add additions

---
+++ src/android/com.mapbox.maps/base-ndk27/binding/Additions/Fixes.cs
@@ -0,0 +1,23 @@
+using Com.Mapbox.Geojson;
+using Com.Mapbox.Bindto;
+
+namespace Com.Mapbox.Maps.Interactions.Standard.Generated
+{
+    public partial class StandardPoi
+    {
+        public override Java.Lang.Object GetFeaturesetFeature(Feature f, string s, Value v) => null;
+        public override Java.Lang.Object GetFeatureState(Value v) => null;
+    }
+    
+    public partial class StandardPlaceLabels
+    {
+        public override Java.Lang.Object GetFeaturesetFeature(Feature f, string s, Value v) => null;
+        public override Java.Lang.Object GetFeatureState(Value v) => null;
+    }
+    
+    public partial class StandardBuildings
+    {
+        public override Java.Lang.Object GetFeaturesetFeature(Feature f, string s, Value v) => null;
+        public override Java.Lang.Object GetFeatureState(Value v) => null;
+    }
+}
